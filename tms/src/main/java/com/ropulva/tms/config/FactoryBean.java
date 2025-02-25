c60e1d47b629a660e61f520e65f78a8bf293a7f6
MohamedYehia20m <medoyehia2001@gmail.com>
2025-02-25 21:23:56 +0200

    feat(config) : init Factory bean for modelmapper

---
+++ tms/src/main/java/com/ropulva/tms/config/FactoryBean.java
@@ -0,0 +1,13 @@
+package com.ropulva.tms.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class FactoryBean {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
