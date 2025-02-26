874a47604a6ca5b4c3efadcbf034a64cdfcc1f86
MohamedYehia20m <medoyehia2001@gmail.com>
2025-02-26 21:23:09 +0200

    refactor(user) : modify 'nullable' attribute to enable creating User Account with phone,email

--- tms/src/main/java/com/ropulva/tms/model/User.java
+++ tms/src/main/java/com/ropulva/tms/model/User.java
@@ -12,14 +12,14 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String username;
 
     @ManyToOne
-    @JoinColumn(name = "workspace_id" , nullable = false)
+    @JoinColumn(name = "workspace_id")
     private Workspace workspace;
 
-    @Column(length = 11)
+    @Column(nullable = false , length = 11)
     private String phone;
 
     @Column(nullable = false, unique = true)
