3fd89795d953a615a475d6be240f6cc7449184a2
MohamedYehia20m <medoyehia2001@gmail.com>
2025-02-28 01:24:53 +0200

    refactor(task) : refactor for better exception handling and logging mechanism

--- tms/src/main/java/com/ropulva/tms/model/Task.java
+++ tms/src/main/java/com/ropulva/tms/model/Task.java
@@ -13,7 +13,7 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false , unique = true)
     private String title;
 
     private String description;
--- tms/src/main/java/com/ropulva/tms/service/TaskServiceImpl.java
+++ tms/src/main/java/com/ropulva/tms/service/TaskServiceImpl.java
@@ -5,15 +5,15 @@ import com.ropulva.tms.dto.TaskSaveDto;
 import com.ropulva.tms.enums.Status;
 import com.ropulva.tms.model.Task;
 import com.ropulva.tms.repository.TaskRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Optional;
+
 
 @RequiredArgsConstructor
 @Service
@@ -22,128 +22,76 @@ public class TaskServiceImpl implements ITaskService {
     final ModelMapper modelMapper;
 
     public ResponseEntity<List<TaskDto>> getTasks() {
-        try{
-            List<Task> tasks = taskRepository.findAll();
-            return ResponseEntity.status(200).body(tasks.stream().map(task -> modelMapper.map(task, TaskDto.class)).toList());
-        }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(500).build();
-        }
+        List<Task> tasks = taskRepository.findAll();
+        return ResponseEntity.ok(tasks.stream().map(task -> modelMapper.map(task, TaskDto.class)).toList());
     }
 
     public ResponseEntity<TaskDto> getTask(Long id) {
-        try {
-            Optional<Task> taskOptional = taskRepository.findById(id);
-            if (taskOptional.isPresent()) {
-                Task task = taskOptional.get();
-                return ResponseEntity.status(200).body(modelMapper.map(task, TaskDto.class));
-            } else {
-                return ResponseEntity.status(404).build();
-            }
-        }
-        catch (DataIntegrityViolationException | OptimisticLockingFailureException e) {
-            return ResponseEntity.status(409).build();
-        }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(500).build();
-        }
+            Task task = taskRepository.findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("Task not found with id: " + id));
+            return ResponseEntity.ok(modelMapper.map(task, TaskDto.class));
     }
 
     public ResponseEntity<TaskDto> createTask(TaskDto taskDto) {
         try {
             Task task = taskRepository.save(modelMapper.map(taskDto, Task.class));
-            return ResponseEntity.status(201).body(modelMapper.map(task, TaskDto.class));
+            return ResponseEntity.ok(modelMapper.map(task, TaskDto.class));
         }
         catch (IllegalArgumentException e) {
-            return ResponseEntity.status(400).build();
+            throw new IllegalArgumentException("Invalid task data, illegal argument");
         }
         catch (IllegalStateException e) {
-            return ResponseEntity.status(409).build();
+            throw new IllegalStateException("Invalid task data");
         }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(500).build();
+        catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Invalid task data, constraint violation ");
+        }
+        catch (OptimisticLockingFailureException e) {
+            throw new OptimisticLockingFailureException("Invalid task data, optimistic locking failure");
         }
     }
 
     public ResponseEntity<TaskDto> updateTask(TaskSaveDto taskSaveDto) {
         try {
-            Optional<Task> taskOptional = taskRepository.findById(taskSaveDto.getId());
-            if (taskOptional.isPresent()) {
-                Task updatedTask = taskRepository.save(modelMapper.map(taskSaveDto, Task.class));
-                return ResponseEntity.status(200).body(modelMapper.map(updatedTask, TaskDto.class));
-            } else {
-                return ResponseEntity.status(404).build();
-            }
-        }
-        catch (DataIntegrityViolationException | OptimisticLockingFailureException | IllegalStateException e) {
-            return ResponseEntity.status(409).build();
+            Task task = taskRepository.findById(taskSaveDto.getId())
+                    .orElseThrow(() -> new EntityNotFoundException("Task not found with id: " + taskSaveDto.getId()));
+            Task updatedTask = taskRepository.save(task);
+            return ResponseEntity.status(200).body(modelMapper.map(updatedTask, TaskDto.class));
         }
         catch (IllegalArgumentException e) {
-            return ResponseEntity.status(400).build();
+            throw new IllegalArgumentException("Invalid task data");
         }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(500).build();
+        catch (IllegalStateException e) {
+            throw new IllegalStateException("Invalid task data");
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Invalid task data, constraint violation");
+        }
+        catch (OptimisticLockingFailureException e) {
+            throw new OptimisticLockingFailureException("Invalid task data, optimistic locking failure");
         }
     }
 
     public ResponseEntity<TaskDto> updateTaskDone(Long id) {
-        try {
-            Optional<Task> taskOptional = taskRepository.findById(id);
-            if (taskOptional.isPresent()) {
-                Task task = taskOptional.get();
-                task.setStatus(Status.DONE);
-                Task updatedTask = taskRepository.save(task);
-                return ResponseEntity.status(200).body(modelMapper.map(updatedTask, TaskDto.class));
-            } else {
-                return ResponseEntity.status(404).build();
-            }
-        }
-        catch (DataIntegrityViolationException | OptimisticLockingFailureException e) {
-            return ResponseEntity.status(409).build();
-        }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(500).build();
-        }
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Task not found with id: " + id));
+        task.setStatus(Status.DONE);
+        Task updatedTask = taskRepository.save(task);
+        return ResponseEntity.status(200).body(modelMapper.map(updatedTask, TaskDto.class));
     }
 
     public ResponseEntity<TaskDto> updateTaskUndone(Long id) {
-        try {
-            Optional<Task> taskOptional = taskRepository.findById(id);
-            if (taskOptional.isPresent()) {
-                Task task = taskOptional.get();
-                task.setStatus(Status.PENDING);
-                Task updatedTask = taskRepository.save(task);
-                return ResponseEntity.status(200).body(modelMapper.map(updatedTask, TaskDto.class));
-            } else {
-                return ResponseEntity.status(404).build();
-            }
-        }
-        catch (DataIntegrityViolationException | OptimisticLockingFailureException e) {
-            return ResponseEntity.status(409).build();
-        }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(500).build();
-        }
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Task not found with id: " + id));
+        task.setStatus(Status.PENDING);
+        Task updatedTask = taskRepository.save(task);
+        return ResponseEntity.status(200).body(modelMapper.map(updatedTask, TaskDto.class));
     }
 
     public ResponseEntity<Void> deleteTask(Long id) {
-        try {
-            Optional<Task> taskOptional  = taskRepository.findById(id);
-            if (taskOptional.isPresent()) {
-                taskRepository.delete(taskOptional.get());
-                return ResponseEntity.status(204).build();
-            } else {
-                return ResponseEntity.status(404).build();
-            }
-        }
-        catch (DataIntegrityViolationException | OptimisticLockingFailureException e) {
-            return ResponseEntity.status(409).build();
-        }
-        catch (IllegalArgumentException e) {
-            return ResponseEntity.status(400).build();
-        }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(500).build();
-        }
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Task not found with id: " + id));
+        taskRepository.delete(task);
+        return ResponseEntity.noContent().build();
     }
 }
